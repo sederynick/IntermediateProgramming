08f45453ca0e4929bfec70997288f0b5f3fb5d91
hmanness <hmanness@B-1100-26.ggc.edu>
2017-10-03 20:18:40 -0400

    check

--- ITEC2150/src/chapter13/temperaturescaleconversion/TemperatureScaleConverter.java
+++ ITEC2150/src/chapter13/temperaturescaleconversion/TemperatureScaleConverter.java
@@ -2,6 +2,21 @@ package chapter13.temperaturescaleconversion;
 
 import java.awt.event.ActionListener;
 
-public interface TemperatureScaleConverter extends ActionListener {
-
+public interface TemperatureScaleConverter 
+extends ActionListener {
+	
+	//public static final keywords are optional
+	int WINDOW_WIDTH=350;
+	int WINDOW_HEIGHT=250;
+	
+	//by default all methods are abstract in an interface
+	double celciusToFahrenheit(double temperature);
+	double celciusToKelvin(double temperature);
+	double fahrenheitToCelcius(double temperature);
+	double fahrenheitToKelvin(double temperature);
+	double kelvinToCelcius(double temperature);
+	double kelvinToFahrenheit(double temperature);
+	
+	
+	
 }
