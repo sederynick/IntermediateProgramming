d0798a7b5b22e9f9577df7fdb8acbb2b0c89cad6
Harley <hmanness@ggc.edu>
2017-10-04 13:20:56 -0400

    modular calculator personal project with GUI

---
+++ ITEC2150/src/modularCalculator/ModularCalculatorGUI.java
@@ -0,0 +1,32 @@
+package modularCalculator;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.JFrame;
+
+public class ModularCalculatorGUI implements ModualrCalculatorInterface {
+	
+	private int mod;
+	private int input;
+	
+	private JFrame frame;
+	private 
+	
+
+	public ModularCalculatorGUI() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public double modCalculation(int mod, int input) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
